3a4c4e3bcb72032147eff2afe7bbaef2dde0b0ad
JanioAbrantes <janioabrantes@gmail.com>
2023-01-30 23:40:47 -0300

    Creating some login page tests

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,19 +1,24 @@
-const faker = require("faker");
+import { faker } from '@faker-js/faker';
 
 describe('Irá testar as funcionalidades da página de Login', () => {
     beforeEach(() => {
         cy.visit('https://barrigareact.wcaquino.me/')
     })
-    it.only('lalala', () => {
+    it('The user click on "entrar" button with all fields empty', () => {
+        // This test will fail
+        cy.get('.btn').click()
+        cy.get('.toast-message').should('contain', 'status code 40')
+    })
+    it('The user click on "entrar" button with the password field empty', () => {
         const email_field = '[data-test="email"]'
-        cy.get('contain', 'email').type(faker.Internet.email())
+        cy.get(email_field).type(faker.internet.email())
+        cy.get('.btn').click()
+        cy.get('.toast-message').should('contain', 'status code 40')
     })
-    it('lalala3', () => {
-        let titulo = cy.title();
-        cy.log(titulo);
-    })
-    it('lalala5', () => {
-        let titulo = cy.title();
-        cy.log(titulo);
+    it('The user click on "entrar" button with the email field empty', () => {
+        const password_field = '[data-test="passwd"]'
+        cy.get(password_field).type(faker.random.alphaNumeric(8))
+        cy.get('.btn').click()
+        cy.get('.toast-message').should('contain', 'status code 40')
     })
 })
